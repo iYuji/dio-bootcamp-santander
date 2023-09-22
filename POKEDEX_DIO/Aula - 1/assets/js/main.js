c362bb914d4533a358cb3761ddca633180916b1d
iYuji <raphaelyujinakano@icloud.com>
2023-09-22 02:00:34 -0300

    pokedex-dio

---
+++ POKEDEX_DIO/Aula - 1/assets/js/main.js
@@ -0,0 +1,25 @@
+
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class="pokemon ${pokemon.type}">
+    <span class="number">#${pokemon.number}</span>
+    <span class="name">${pokemon.name}</span>
+
+    <div class="detail">
+        <ol class="types">
+           ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+        </ol>
+
+             <img src="${pokemon.photo}" 
+                alt="${pokemon.name}">
+    </div>
+    </li>
+    `;
+}
+
+const pokemonList = document.getElementById("pokemonList");
+
+//requisição da API, transformando o retorno em uma Li, e concatenando ao HTML
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join("");
+});
