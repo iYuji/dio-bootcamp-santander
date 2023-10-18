4a070b2f6cf33929adc1d8f98ec5e593a8ed4f9e
iYuji <raphaelyujinakano@icloud.com>
2023-10-18 01:19:00 -0300

    Atualização dos Components em CardNews

--- Angular/src/components/CardNews.js
+++ Angular/src/components/CardNews.js
@@ -1,10 +1,49 @@
-class CardNews extends HTMLElement {
+class Cardnews extends HTMLElement{
     constructor(){
         super();
-
         const shadow = this.attachShadow({mode:"open"});
-        shadow.innerHTML = "<h1>Hello World!</h1>";
-    }
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
 }
 
-customElements.define("card-news", CardNews);
+build(){
+    const componentRoot = document.createElement("div");
+    componentRoot.setAttribute("class", "card");
+
+    const cardLeft = document.createElement("div");
+    cardLeft.setAttribute=this.setAttribute("class", "card_left")
+
+    const autor = document.createElement("span");
+    autor.textContent = "By " + (this.getAttribute("autor") || " Anonymous");
+
+    const linkTitle = document.createElement("a");
+    linkTitle.textContent = this.getAttribute("title")
+
+    const newsContent = document.createElement("p");
+    newsContent.textContent = this.getAttribute("content");
+
+    
+    
+    cardLeft.appendChild(autor);
+    cardLeft.appendChild(linkTitle);
+    cardLeft.appendChild(newsContent);
+
+    const cardright = document.createElement("div");
+    cardright.setAttribute=this.setAttribute("class", "card_right")
+
+    const newsImage = document.createElement("img");
+    cardright.appendChild(newsImage);
+
+    componentRoot.appendChild(cardLeft);
+    componentRoot.appendChild(cardright);
+
+
+    return componentRoot;
+}
+
+styles(){
+    
+}
+
+}
+customElements.define("card-news", Cardnews);
